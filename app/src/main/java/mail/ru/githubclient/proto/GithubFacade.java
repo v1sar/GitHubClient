bc42d9b244f5165b29d3cd1e7f3bf3b9f7176ae4
Nikita <nks.off@gmail.com>
2016-12-01 20:10:43 +0300

    add github interface

--- app/src/main/java/mail/ru/githubclient/proto/GithubFacade.java
+++ app/src/main/java/mail/ru/githubclient/proto/GithubFacade.java
@@ -4,10 +4,19 @@ import org.androidannotations.annotations.EBean;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 @EBean(scope = EBean.Scope.Singleton)
 public class GithubFacade {
 
     public static final String API_URL = "https://api.github.com";
+
+    public interface GitHub {
+        @GET("/search/users?q={query}")
+        Call<List<User>> users(@Path("query") String query);
+    }
 
     public void getUsers(String name, Callback<List<User>> users) {
         //TODO
@@ -23,6 +32,7 @@ public class GithubFacade {
 
     public interface Callback<T> {
         void onSuccess(T result);
+
         void onError(Exception e);
     }
 
