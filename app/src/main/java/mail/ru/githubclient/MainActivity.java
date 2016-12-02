6650a75a4e95f181c6ab3ef52892d3f4b618768e
v1sar <v1sar@mail.ru>
2016-12-02 03:10:49 +0300

    added authorization

--- app/src/main/java/mail/ru/githubclient/MainActivity.java
+++ app/src/main/java/mail/ru/githubclient/MainActivity.java
@@ -1,17 +1,26 @@
 package mail.ru.githubclient;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.util.Base64;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,6 +30,7 @@ import mail.ru.githubclient.reflect.Proxies;
 import mail.ru.githubclient.reflect.Wrapper;
 
 @EActivity(R.layout.activity_main)
+@OptionsMenu({R.menu.menu_main})
 public class MainActivity extends AppCompatActivity {
 
     private final List<Wrapper<?>> wrappers = new ArrayList<>();
@@ -37,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
     @ViewById(R.id.users)
     RecyclerView users;
 
+    @ViewById(R.id.toolbar)
+    Toolbar toolbar;
+
     private GithubFacade.Callback<List<User>> getUsersCallback;
+    private GithubFacade.Callback<User> getOAuthCallback;
 
     @AfterViews
     void initCallbacks() {
@@ -53,9 +67,25 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
+        GithubFacade.Callback<User> callbackOAuth = new GithubFacade.Callback<User>() {
+            @Override
+            public void onSuccess(User result) {
+                Toast.makeText(MainActivity.this, "Hello "+result.getLogin(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Exception e) {
+
+            }
+        };
+
         Wrapper<GithubFacade.Callback> wrapper = Proxies.wrap(GithubFacade.Callback.class, callback);
         wrappers.add(wrapper);
         getUsersCallback = wrapper.delegate();
+
+        Wrapper<GithubFacade.Callback> wrapper1 = Proxies.wrap(GithubFacade.Callback.class, callbackOAuth);
+        wrappers.add(wrapper1);
+        getOAuthCallback = wrapper1.delegate();
     }
 
     @AfterViews
@@ -63,13 +93,48 @@ public class MainActivity extends AppCompatActivity {
         // Setup adapter here
         users.setLayoutManager(new LinearLayoutManager(this));
         users.setAdapter(adapter);
+        setSupportActionBar(toolbar);
     }
+
 
     @Click(R.id.search)
     void onSearchClick() {
         // Perform search
-
         facade.getUsers(searchName.getText().toString(), getUsersCallback);
+    }
+
+    @OptionsItem
+    void actionLogin() {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Login to github");
+        alert.setMessage("Provide your login and password");
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        final EditText inputLogin = new EditText(this);
+        inputLogin.setHint("Login");
+        final EditText inputPassword = new EditText(this);
+        inputPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        inputPassword.setHint("Password");
+        layout.addView(inputLogin);
+        layout.addView(inputPassword);
+        alert.setView(layout);
+        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String userPass = inputLogin.getText()+":"+inputPassword.getText();
+                byte[] data = null;
+                try {
+                    data = userPass.getBytes("UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                facade.getOAuth(Base64.encodeToString(data, Base64.DEFAULT), getOAuthCallback);
+            }
+        });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+        alert.show();
     }
 
     @Override
--- app/src/main/java/mail/ru/githubclient/proto/GithubFacade.java
+++ app/src/main/java/mail/ru/githubclient/proto/GithubFacade.java
@@ -1,6 +1,7 @@
 package mail.ru.githubclient.proto;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EBean;
@@ -8,16 +9,22 @@ import org.androidannotations.annotations.EBean;
 import java.io.IOException;
 import java.util.List;
 
+import mail.ru.githubclient.MainActivity;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Retrofit;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 @EBean(scope = EBean.Scope.Singleton)
 public class GithubFacade {
 
     private static final String API_URL = "https://api.github.com";
+    private static String oAuth;
+    private static User loggedUser;
 
     private Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(API_URL)
@@ -27,6 +34,8 @@ public class GithubFacade {
     public interface GitHub {
         @GET("/search/users")
         Call<UsersResult> users(@Query("q") String query);
+        @GET("user")
+        Call<User> getOAuth(@Header("Authorization") String authorization);
     }
 
     private GitHub github = retrofit.create(GitHub.class);
@@ -45,6 +54,26 @@ public class GithubFacade {
         catch(IOException e) {
             users.onError(e);
         }
+    }
+
+    @Background
+    public void getOAuth(String base64, final Callback<User> user) {
+        oAuth = "Basic "+base64.trim();
+        Call call = github.getOAuth(oAuth);
+        call.enqueue(new retrofit2.Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                if (response.errorBody() == null) {
+                    user.onSuccess((User) response.body());
+                    loggedUser = (User) response.body();
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Log.d("Facade", t.getMessage());
+            }
+        });
     }
 
     public void getRepos(User user, Callback<List<Repo>> repos) {
